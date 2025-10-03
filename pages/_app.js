7d4c8d885fb7701f0474369c758174f650451872
SWAYAM GUPTA <gswayam94@gmail.com>
2025-10-03 15:40:04 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import '../styles/globals.css';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({ duration: 1200 });
+  }, []);
+  
   return <Component {...pageProps} />;
 }
 
