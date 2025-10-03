92337b4c9d02ba3fb8bfbbdba286453208e47dae
SWAYAM GUPTA <gswayam94@gmail.com>
2025-10-03 12:29:21 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
+import Achievements from "@/components/Achievements";
+import Contact from "@/components/Contact";
+import ScrollToTop from "@/components/ScrollToTop";
+
+export default function Home() {
+  return (
+    <div className="bg-gray-950 text-white scroll-smooth">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Achievements />
+      <Contact />
+      <ScrollToTop />
+    </div>
+  );
+}
